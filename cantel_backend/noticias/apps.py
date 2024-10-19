4a4152b4ed1de355f6fb4de6a2e77764aecc08d5
Henrysd74 <103168861+Henrysd74@users.noreply.github.com>
2024-10-18 19:33:19 -0600

    Update apps.py

--- cantel_backend/noticias/apps.py
+++ cantel_backend/noticias/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class NoticiasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cantel_backend.cantel_backend.noticias'
+    name = 'cantel_backend.noticias'
